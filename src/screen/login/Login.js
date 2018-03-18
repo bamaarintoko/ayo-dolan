c4ce0ed5cb80284073549d00edce9cb4ffc41bae
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-03-18 22:41:14 +0700

    change UI login

--- src/screen/login/Login.js
+++ src/screen/login/Login.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {StyleSheet, View, AsyncStorage, Image, StatusBar} from "react-native";
+import {StyleSheet, View, AsyncStorage, Image, StatusBar, TouchableWithoutFeedback} from "react-native";
 import {Button, Input, Item, Text} from "native-base";
 // import { Field, reduxForm } from "redux-form";
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -10,8 +10,9 @@ import md5 from 'crypto-js/md5';
 import Swiper from 'react-native-swiper'
 import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
 import {normalize, normalizeFont} from "../../utils/func";
-import {Alert, Keyboard} from "react-native";
+import {Alert, Keyboard, TouchableHighlight} from "react-native";
 import {actLogin} from "./action";
+import * as Animatable from 'react-native-animatable';
 
 let styles = {
     wrapper: {},
@@ -30,7 +31,7 @@ let styles = {
     slide3: {
         flex: 1,
         flexDirection: 'column',
-        backgroundColor: '#FFFFFF'
+        backgroundColor: '#2196F3'
     },
     text: {
         color: '#fff',
@@ -46,10 +47,10 @@ let styles = {
         flex: 1,
         borderWidth: StyleSheet.hairlineWidth,
         height: StyleSheet.hairlineWidth,
-        borderColor: '#9B9FA4'
+        borderColor: '#FFFFFF'
     },
     separatorOr: {
-        color: '#9B9FA4',
+        color: '#FFFFFF',
         marginHorizontal: 8,
         fontSize: normalizeFont(4 * .5)
     },
@@ -61,32 +62,36 @@ class Login extends Component {
         this.state = {
             username: '',
             password: '',
-            flexLogo:1,
-            hideButtonFb:false
+            flexLogo: 1,
+            hideButtonFb: false
         }
         // this.login = this.login.bind(this)
     }
 
-    componentWillMount () {
+
+
+    componentWillMount() {
         this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this._keyboardDidShow);
         this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide);
     }
-    componentWillUnmount () {
+
+    componentWillUnmount() {
         this.keyboardDidShowListener.remove();
         this.keyboardDidHideListener.remove();
     }
-    _keyboardDidShow= ()=> {
+
+    _keyboardDidShow = () => {
         this.setState({
-            flexLogo:0,
-            hideButtonFb:true
+            flexLogo: 0,
+            hideButtonFb: true
         })
         // alert('Keyboard Shown');
     }
 
-    _keyboardDidHide= ()=> {
+    _keyboardDidHide = () => {
         this.setState({
-            flexLogo:1,
-            hideButtonFb:false
+            flexLogo: 1,
+            hideButtonFb: false
         })
         // alert('Keyboard Hidden');
     }
@@ -130,76 +135,89 @@ class Login extends Component {
 
     render() {
         return (
+
             <Swiper style={styles.wrapper} loop={false}>
+
                 <View style={styles.slide1}>
-                    <Text style={styles.text}>Hello Swiper</Text>
+                    <StatusBar
+                        backgroundColor="#4FC3F7"
+                        barStyle="light-content"
+                    />
+                    <Text>Haloo</Text>
                 </View>
                 <View style={styles.slide2}>
-                    <Text style={styles.text}>Beautiful</Text>
+                    <Text>Halloo</Text>
                 </View>
                 <View style={styles.slide3}>
-                        <View style={{
-                            flex: this.state.flexLogo,
-                            backgroundColor: '#FFFFFF',
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                        }}>
-                            <Image
-                                resizeMode={'contain'}
-                                style={{width: normalize(130 * .9)}}
-                                source={require('../../utils/assetss/header.png')}
-                            />
+                    <View style={{
+                        flex: this.state.flexLogo,
+                        backgroundColor: '#2196F3',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}>
+                        <Image
+                            resizeMode={'contain'}
+                            style={{width: normalize(130 * .9)}}
+                            source={require('../../utils/assetss/header.png')}
+                        />
+                    </View>
+                    <View style={{flex: 2, paddingLeft: 30, paddingRight: 30}}>
+                        <Item rounded style={{backgroundColor:'#FFFFFF'}}>
+                            <View style={{width: 25}}>
+                                <Icon style={{paddingLeft: 10}} size={normalizeFont(4 * .5)} active
+                                      name='envelope'/>
+                            </View>
+                            <Input onChangeText={this.onChange('username')}
+                                   style={{height: 40, fontSize: normalizeFont(4 * .5)}} placeholder='Email'/>
+                        </Item>
+                        <Item style={{marginTop: 10,backgroundColor:'#FFFFFF'}} rounded>
+                            <View style={{width: 25, alignItems: 'center'}}>
+                                <Icon style={{paddingLeft: 10}} size={normalizeFont(4 * .5)} active name='lock'/>
+                            </View>
+                            <Input onChangeText={this.onChange('password')}
+                                   style={{height: 40, fontSize: normalizeFont(4 * .5)}} placeholder='Password'/>
+                        </Item>
+                        <View style={{flexDirection: 'row', alignItems: 'center', height: 40}}>
+                            <View style={{flex: 1}}>
+                                <TouchableWithoutFeedback style={{paddingTop: 10, paddingBottom: 10}}
+                                                    onPress={() => console.log("asu")}>
+                                    <Text style={{color:'#FFFFFF',fontSize: normalizeFont(4 * .5)}}>Create Account</Text>
+                                </TouchableWithoutFeedback>
+
+                            </View>
+                            <View style={{flex: 1, alignItems: 'flex-end'}}>
+                                <TouchableWithoutFeedback style={{paddingTop: 10, paddingBottom: 10}}
+                                                    onPress={() => console.log("asu")}>
+                                    <Text style={{color:'#FFFFFF',fontSize: normalizeFont(4 * .5)}}>Forgot Password?</Text>
+                                </TouchableWithoutFeedback>
+                            </View>
                         </View>
-                        <View style={{flex: 2, paddingLeft: 30, paddingRight: 30}}>
-                            <Item rounded>
-                                <View style={{width: 25}}>
-                                    <Icon style={{paddingLeft: 10}} size={normalizeFont(4 * .5)} active
-                                          name='envelope'/>
+                        <Button onPress={this.onLogin} small block info>
+                            <Text>Login</Text>
+                        </Button>
+                        {
+                            !this.state.hideButtonFb
+                            &&
+                            <View>
+                                <View style={styles.separatorContainer}>
+                                    <View style={styles.separatorLine}/>
+                                    <Text style={styles.separatorOr}>Or Login With</Text>
+                                    <View style={styles.separatorLine}/>
                                 </View>
-                                <Input onChangeText={this.onChange('username')}
-                                       style={{height: 40, fontSize: normalizeFont(4 * .5)}} placeholder='Email'/>
-                            </Item>
-                            <Item style={{marginTop: 10}} rounded>
-                                <View style={{width: 25, alignItems: 'center'}}>
-                                    <Icon style={{paddingLeft: 10}} size={normalizeFont(4 * .5)} active name='lock'/>
-                                </View>
-                                <Input onChangeText={this.onChange('password')}
-                                       style={{height: 40, fontSize: normalizeFont(4 * .5)}} placeholder='Password'/>
-                            </Item>
-                            <View style={{flex: 0.7, flexDirection: 'row', alignItems: 'center'}}>
-                                <View style={{flex: 1}}>
-                                    <Text style={{fontSize: normalizeFont(4 * .5)}}>Create Account</Text>
-                                </View>
-                                <View style={{flex: 1, alignItems: 'flex-end'}}>
-                                    <Text style={{fontSize: normalizeFont(4 * .5)}}>Forgot Password?</Text>
+                                <View style={{flex: 1, flexDirection: 'row'}}>
+                                    <View style={{flex: 1}}>
+                                        <Button onPress={this.login} style={{marginTop: 2, backgroundColor: '#3b5998'}}
+                                                small
+                                                block info>
+                                            <Text><Icon style={{paddingLeft: 10}} size={normalizeFont(4 * .5)} active
+                                                        name='facebook'/></Text>
+                                        </Button>
+                                    </View>
                                 </View>
                             </View>
-                            <Button onPress={this.onLogin} small block info>
-                                <Text>Login</Text>
-                            </Button>
-                            {
-                                !this.state.hideButtonFb
-                                &&
-                                    <View>
-                                        <View style={styles.separatorContainer}>
-                                            <View style={styles.separatorLine}/>
-                                            <Text style={styles.separatorOr}>Or Login With</Text>
-                                            <View style={styles.separatorLine}/>
-                                        </View>
-                                        <View style={{flex: 1, flexDirection: 'row'}}>
-                                            <View style={{flex: 1}}>
-                                                <Button onPress={this.login} style={{marginTop: 2, backgroundColor: '#3b5998'}}
-                                                        small
-                                                        block info>
-                                                    <Text><Icon style={{paddingLeft: 10}} size={normalizeFont(4 * .5)} active
-                                                                name='facebook'/></Text>
-                                                </Button>
-                                            </View>
-                                        </View>
-                                    </View>
-                            }
+                        }
 
-                        </View>
+                    </View>
                 </View>
             </Swiper>
         );
