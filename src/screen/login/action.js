7ec937adac7883de32f00dcbe6eabfac7443e690
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-03-23 16:54:26 +0700

    add redux persist

--- src/screen/login/action.js
+++ src/screen/login/action.js
@@ -7,8 +7,14 @@ export function actLogin(params) {
             .then((response)=>{
                 if (response.data.status){
                     dispatch({type:'HOME'})
+                    dispatch({
+                        type: 'LOGIN_SUCCESS',
+                        status_login: true,
+                        data: response.data.results[0],
+                        message: "login sukses"
+                    })
                 }
-                console.log(response)
+                console.log(response.data)
             }).catch((error)=>{
                 console.log(error)
         })
