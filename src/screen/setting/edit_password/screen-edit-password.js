1a19742fe5f6fd1ec2d11ae4e0f96f80c0d55071
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-03-25 20:26:47 +0700

    add header

--- src/screen/setting/edit_password/screen-edit-password.js
+++ src/screen/setting/edit_password/screen-edit-password.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Container} from "native-base";
+import Head from "../../../Components/Head";
 
 
 class ViewEditPassword extends Component {
     render() {
         return (
             <Container>
-
+                <Head
+                    body={"Edit Password"}
+                    leftIcon={'arrow-left'}
+                    leftPress={() => this.props.navigation.goBack()}
+                    rightPress={() => this.props.navigation.navigate('DetailMessage')}
+                />
             </Container>
         );
     }
--- src/screen/setting/edit_profil/screen-edit-profil.js
+++ src/screen/setting/edit_profil/screen-edit-profil.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Container} from "native-base";
+import Head from "../../../Components/Head";
 
 class ViewEditProfile extends Component {
     render() {
         return (
             <Container>
-                
+                <Head
+                    body={"Edit Profil"}
+                    leftIcon={'arrow-left'}
+                    leftPress={() => this.props.navigation.goBack()}
+                    rightPress={() => this.props.navigation.navigate('DetailMessage')}
+                />
             </Container>
         );
     }
